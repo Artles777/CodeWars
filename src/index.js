67dc6445ce0918cf0483282e270b8e2ed2b0b083
Artem Alesenko <62352758+Artles777@users.noreply.github.com>
2021-06-04 09:33:49 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function palendrom(word) {
+  const arrWord = word.toLowerCase().split("");
+  return (
+    JSON.stringify(arrWord.filter((w, i) => i < arrWord.length / 2)) ===
+    JSON.stringify(arrWord.slice(arrWord.length / 2, arrWord.length).reverse())
+  );
+}
+
+console.log(palendrom("aaBBaa"));
