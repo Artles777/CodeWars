b8d87e616c2e64c66fe7488e21f8215dbf760674
Artem Alesenko <62352758+Artles777@users.noreply.github.com>
2021-06-11 21:18:09 +0300

    added 3 task

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import palindrom from "./tasks/1_palindrom";
 import sortArr from "./tasks/2_sortUnique";
+import integerValues from "./tasks/3_multiplyByTwo";
 
 console.log(palindrom("aaBBaa"));
 
@@ -20,5 +21,8 @@ const array = [
   "persik"
 ];
 
+const nums = [1, 2, 3, 4, 5, 6, 7, 8];
+
 console.log(palindrom("aaBBaa"));
 console.log(sortArr(array));
+console.log(integerValues(nums));
---
+++ src/tasks/3_integerValues.js
@@ -0,0 +1,5 @@
+function integerValues(arr) {
+  return arr.filter((el) => (el % 2 ? false : true));
+}
+
+export default integerValues;
